2547862b6826b798f720b0b726ed90ca48b0d403
Vitaliy Zinchenko <vitaliy.zinchenko@utasco.com>
2019-07-30 15:37:26 +0300

    Added documentation
    Updated README.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # sbench
 Simple benchmark for Rust
+
+## Examples
+```rust
+let t = sbench::bench(|| {
+    let a = 1;
+    let b = 2;
+    let _ = a + b;
+});
+
+assert!(t.as_nanos() > 0);
+```
+```rust
+let t = sbench::bench_n(10, || {
+    let a = 1;
+    let b = 2;
+    let _ = a + b;
+});
+
+assert!(t.as_nanos() > 0);
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,38 @@
+//! sbench is a simple benchmark for Rust. 
+//! It allows you to measure the performance of functions.
+
 use std::time::{Duration, Instant};
 
+/// Measures the performance of a function by running it 1000 times.
+/// 
+/// # Example
+/// ```rust
+/// let t = sbench::bench(|| {
+///     let a = 1;
+///     let b = 2;
+///     let _ = a + b;
+/// });
+/// 
+/// assert!(t.as_nanos() > 0);
+/// ```
 pub fn bench<T>(f: T) -> Duration
     where T: Fn()
 {
     bench_n(1000, f)
 }
 
+/// Measures the performance of the function, running it as many times as indicated in the parameter `times`.
+/// 
+/// # Example
+/// ```rust
+/// let t = sbench::bench_n(1_000_000, || {
+///     let a = 1;
+///     let b = 2;
+///     let _ = a + b;
+/// });
+/// 
+/// assert!(t.as_nanos() > 0);
+/// ```
 pub fn bench_n<T>(times: usize, f: T) -> Duration
     where T: Fn()
 {
